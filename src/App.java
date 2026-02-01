3a5c7629cb6c88cc55fdbda7c49b6a0476a1795a
moreno_esteban <esteban.roa@correounivalle.edu.co>
2026-02-01 18:13:20 -0500

    primera entrega finalizada como requisito de certificacion

--- src/App.java
+++ src/App.java
@@ -2,32 +2,49 @@ import java.util.Scanner;
 
 public class App {
 
+    //variables declaradas de forma global.
     static int seleccion;
     static Scanner sc = new Scanner(System.in);
     static String nombreProducto;
     static double precioUnitario;
     static int cantidadInventario;
+    static double costeInventario;
+    static String estadoproducto;
+
+    //funcion principal control de flujo del programa.
     public static void main(String[] args) throws Exception {
-        mostrarMenu();
+        //mostra menu de opciones.
 
-        seleccion = sc.nextInt();
-        sc.nextLine(); // Limpiar el buffer
-        if(seleccion ==1){
-            registrarProducto();
-        }else if(seleccion ==2){
-            mostrarInfoProducto();}
-        
-        System.out.println("Gracias por usar el sistema de gestión de productos.");
+        do {
+            mostrarMenu(args);
+            seleccion = sc.nextInt();
+            sc.nextLine(); //limpiar buffer
 
-        sc.close();
+            switch (seleccion) {
+                case 1 -> registrarProducto();
+                case 2 -> mostrarInfo();
+                case 3 -> valorInventario();
+                case 4 -> resumenCompleto();
+                case 5 -> limpiarDatos();
+                case 0 -> System.out.println("Saliendo del programa...");
+                default -> {System.out.println("Opción inválida. Por favor, seleccione una opción válida del menú.");
+                }
+            }
+
+            
+        } while (seleccion != 0);
+
+
+        sc.close();  
 
     }
-    
 
 
+    //funcion mostrar el menu de opciones.
+    public static void mostrarMenu(String[] args) {
 
-    static void mostrarMenu() {
         var opciones = """
+
             --- Sistema de Gestión de Productos ---
 
             1. Registrar nuevo producto
@@ -36,47 +53,134 @@ public class App {
             4. Mostrar resumen completo del producto
             5. Limpiar datos del producto actual
             0. Salir
-            Ingrese su opción:
-                """;
 
-        System.out.print(opciones);
+            Ingrese su opción:""";
+
+        System.out.println(opciones);
+        
     }
+    //funcion registrar un nuevo producto.
+    public static void registrarProducto() {
 
-    static void registrarProducto() {// Lógica para registrar un nuevo producto
-        if (nombreProducto != null) {
-            System.out.println("Ya hay un producto registrado. Por favor, limpie los datos antes de registrar uno nuevo.");
+        //verificar si ya existe un producto registrado.
+        if(nombreProducto != null) {
+            System.out.println("Ya existe un producto registrado. Por favor, desea sobreescribirlo? (s/n)");
+            String respuesta = sc.nextLine();
+            if(!respuesta.equalsIgnoreCase("s")) {
+                return; //salir de la funcion sin registrar un nuevo producto.
+            }
         }
-        System.out.print("Ingrese el nombre del producto: ");
+        //si no hay un producto leido, solicitar datos del producto.
+        do{System.out.println("Ingrese el nombre del producto:");
         nombreProducto = sc.nextLine();
-        if (nombreProducto.isEmpty()) {
-            System.out.println("El nombre del producto no puede estar vacío. Por favor, ingrese un nombre válido.");
-            nombreProducto = sc.nextLine();
-        }
-        System.out.print("Ingrese el precio unitario del producto: ");
+        //validar nombre del producto
+        }while(!validarnombre(nombreProducto));
+
+        do{System.out.println("Ingrese el precio unitario del producto:");
         precioUnitario = sc.nextDouble();
-        if(precioUnitario < 0){
-            System.out.println("El precio unitario no puede ser negativo. Por favor, ingrese un valor válido.");
-            precioUnitario = sc.nextDouble();
-        }
-        System.out.print("Ingrese la cantidad en inventario del producto: ");
+        }while(!validarPrecio(precioUnitario));
+
+
+        do{System.out.println("Ingrese la cantidad en inventario:");
         cantidadInventario = sc.nextInt();
-        if(cantidadInventario < 0){
-            System.out.println("La cantidad en inventario no puede ser negativa. Por favor, ingrese un valor válido.");
-            cantidadInventario = sc.nextInt();
-        }
-        sc.nextLine(); // Limpiar el buffer
+
+        sc.nextLine(); //limpiar buffer
+
+        }while(!validarCantidad(cantidadInventario));
+
+
+
+            
+        
+
     }
+    //funcion mostrar informacion del producto registrado.
+    public static void mostrarInfo(){
 
+        if(nombreProducto == null) {
+            System.out.println("No hay datos de producto registrado.");
+            return;
+        }
 
-    static void mostrarInfoProducto() {
+        var info = """
+            --- Información del Producto ---
+
+            Nombre del producto: %s
+            Precio unitario: %.2f
+            Cantidad en inventario: %d
+            """.formatted(nombreProducto, precioUnitario, cantidadInventario);
+        System.out.println(info);
+    }
+    //funcion calcular valor total del inventario.
+    public static double valorInventario(){
         if (nombreProducto == null) {
-            System.out.println("No hay ningún producto registrado.");
-        } else {
-            System.out.println("Nombre del producto: " + nombreProducto);
-            System.out.println("Precio unitario: " + precioUnitario);
-            System.out.println("Cantidad en inventario: " + cantidadInventario);
+            System.out.println("No hay datos de producto registrados.");
+            return 0;
         }
+        costeInventario = precioUnitario * cantidadInventario;
+        return costeInventario;
+    }
+    //funcion mostrar resumen completo del producto.
+    public static void resumenCompleto(){
+        if (nombreProducto==null) {
+            System.out.println("No hay datos de producto registrado.");
+            return;
+        }
+        
+        if (cantidadInventario < 5) {
+            estadoproducto = "Stock Bajo";
+        } else if (cantidadInventario >= 20) {
+            estadoproducto = "Stock Alto";
+        } else 
+            estadoproducto = "Stock suficiente";
+        
+
+
+        var resumen = """
+            --- Resumen Completo del Producto ---
+
+            Nombre del producto: %s
+            Precio unitario: %.2f
+            Cantidad en inventario: %d
+            Estado del producto: %s
+            Valor total del inventario: %.2f
+            """.formatted(nombreProducto, precioUnitario, cantidadInventario, estadoproducto, valorInventario() );
+            System.out.println(resumen);
+    }  
+    //funcion limpiar datos del producto actual.
+    public static void limpiarDatos(){
+        nombreProducto = null;
+        precioUnitario = 0;
+        cantidadInventario = 0;
+        costeInventario = 0;
+        estadoproducto = null;
+        System.out.println("Los datos del producto actual han sido borrados exitosamente.");
+    }
+    //funciones de validacion de datos.
+    public static boolean validarPrecio(double precio) {
+        if (precio < 0) {
+            System.out.println("El precio unitario no puede ser negativo. Por favor, ingrese un valor válido.");
+            return false;
+        }
+        return true;
+    }
+    public static boolean validarCantidad(int cantidad) {
+        if (cantidad < 0) {
+            System.out.println("La cantidad en inventario no puede ser negativa. Por favor, ingrese un valor válido.");
+            return false;
+        }
+        return true;
+    }
+    public static boolean validarnombre(String nombre) {
+        if (nombre.isEmpty()) {
+            System.out.println("El nombre del producto no puede estar vacío. Por favor, ingrese un valor válido.");
+            return false;
+        }
+        return true;
+  
     }
 
-    
 }
+    
+
+
